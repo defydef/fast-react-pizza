2c5649e784080b0de0b48b2c500521d0aaf0d7f7
defy <8841170+defydef@users.noreply.github.com>
2023-10-01 21:26:03 +0700

    Display username and redirect to CreateOrder after entering name

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from 'react-redux'
+
 function Username() {
-    return <div className="hidden text-sm font-semibold md:block">Depi</div>
+    const user = useSelector((store) => store.user)
+    return (
+        <div className="hidden text-sm font-semibold md:block">
+            {user.username || ''}
+        </div>
+    )
 }
 
 export default Username
