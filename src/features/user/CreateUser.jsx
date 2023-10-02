099fb8e90192ca408b90683ce1734be0e7bf3e4d
defy <8841170+defydef@users.noreply.github.com>
2023-10-02 07:45:56 +0700

    Conditionally render in Home instead of CreateUser

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import Button from '../../ui/Button'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { updateName } from './userSlice'
 import { useNavigate } from 'react-router-dom'
 
 function CreateUser() {
     const [username, setUsername] = useState('')
-    const user = useSelector((store) => store.user)
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
@@ -20,25 +19,16 @@ function CreateUser() {
 
     return (
         <form onSubmit={handleSubmit}>
-            {user.username ? (
-                <p className="text-md mb-4 text-stone-600 md:text-lg">
-                    👋 Welcome
-                    <span className="font-bold"> {user.username}</span>
-                </p>
-            ) : (
-                <p className="mb-4 text-sm text-stone-600 md:text-base">
-                    👋 Welcome! Please start by telling us your name:
-                </p>
-            )}
-            {!user.username && (
-                <input
-                    type="text"
-                    placeholder="Your full name"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    className="input mb-8 w-72"
-                />
-            )}
+            <p className="mb-4 text-sm text-stone-600 md:text-base">
+                👋 Welcome! Please start by telling us your name:
+            </p>
+            <input
+                type="text"
+                placeholder="Your full name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="input mb-8 w-72"
+            />
 
             {username !== '' && (
                 <div>
